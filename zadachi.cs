bcbabfbed3c6989cd0b6813e0df7873bb9787b71
raiaprogram123 <raiadragolova@gmail.com>
2025-10-17 12:12:01 +0300

    Add files via upload

---
+++ zadachi.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+//double result = double.Parse(Console.ReadLine());
+//if (result>=5.50)
+//{
+//    Console.WriteLine("excellent");
+//}
+//else 
+//{
+//    Console.WriteLine("nqma izhod");
+//}
+
+
+//int num = int.Parse(Console.ReadLine());
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("even");
+//}
+//else
+//{
+//    Console.WriteLine("odd");
+//}
+
+int num = int.Parse(Console.ReadLine());
+if (num<100)
+{
+    Console.WriteLine("Less than 100");
+}
+else if (num<=200)
+{
+    Console.WriteLine("Between 100 and 200");
+}
+else
+{
+    Console.WriteLine("Greater tnan 200");
+}
