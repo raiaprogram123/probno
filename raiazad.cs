36df107f88d2ea240efeb817b1dc24c8cbf564cd
raiaprogram123 <raiadragolova@gmail.com>
2025-10-23 13:57:28 +0300

    Add files via upload

---
+++ raiazad.cs
@@ -0,0 +1,53 @@
+﻿//double eks = double.Parse(Console.ReadLine());
+//int puzel = int.Parse(Console.ReadLine());
+//int kukli = int.Parse(Console.ReadLine());
+//int meche = int.Parse(Console.ReadLine());
+//int min = int.Parse(Console.ReadLine());
+//int kam = int.Parse(Console.ReadLine());
+
+//int sum_igr = puzel + kukli + meche + min + kam;
+//double sum = puzel * 2.6 + kukli * 3 + meche * 4.1 + min * 8.2 + kam * 2;
+
+//if (sum_igr >= 50)
+//{
+//    sum = sum - (sum * 0.25);
+    
+//}
+//double naem = sum * 0.10;
+//double pechalba = sum - naem;
+
+
+//if (pechalba>eks)
+//{
+//    double moneyLeft = pechalba - eks;
+//    Console.WriteLine($"Yes! {moneyLeft}lv left.");
+//}
+//else
+//{
+//    double moneyNeeded = eks - pechalba;
+//    Console.WriteLine($"Not enough money! {moneyNeeded} lv needed ");
+
+//}
+
+
+
+//double film=double.Parse(Console.ReadLine());
+//int broy_statis = int.Parse(Console.ReadLine());
+//double cena_obleklo=double.Parse(Console.ReadLine());
+
+//double dekor = film * 0.10;
+//double obleklo = broy_statis * cena_obleklo;
+//double sum_film = dekor + obleklo;
+
+//if (sum_film>film)
+//{
+//    double moneyNeeds = sum_film - film;
+//    Console.WriteLine("Not enough money!");
+//    Console.WriteLine($"Wingard needs{moneyNeeds}leva more.");
+//}
+//else
+//{
+//    double moneyLeft = film - sum_film;
+//    Console.WriteLine("Action!");
+//    Console.WriteLine($"Wingard starts film with{moneyLeft}leva left.");
+//}
