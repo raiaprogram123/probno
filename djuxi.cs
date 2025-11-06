6c721a60340d56cc55bff2f2511f194dda45e2f2
raiaprogram123 <raiadragolova@gmail.com>
2025-11-06 14:06:08 +0200

    Add files via upload

---
+++ djuxi.cs
@@ -0,0 +1,122 @@
+﻿//int day = int.Parse(Console.ReadLine());
+//switch (day)
+//{
+//    case 1:
+//        Console.WriteLine("Monday");
+//        break;
+//        case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+//        case 3:
+//        Console.WriteLine("Wednesday");
+//        break;
+//        case 4:
+//        Console.WriteLine("Thursday");
+//        break;
+//        case 5:
+//        Console.WriteLine("Friday");
+//        break;
+//        case 6:
+//        Console.WriteLine("Saturday");
+//        break;
+//        case 7:
+//        Console.WriteLine("Sunday");
+//        break;
+//    default:
+//        Console.WriteLine("error");
+//        break;
+//}
+
+//string day=Console.ReadLine();
+//switch (day)
+//{
+//    case "Monday":
+//    case "Tuesday":
+//    case "Wednesday":
+//    case "Thursday":
+//    case "Friday":
+//       Console.WriteLine("Working day");
+//        break;
+//    case "Sarurday":
+//    case "Sunday":
+//        Console.WriteLine("Weekend");
+//        break ;
+//    default:
+//        Console.WriteLine("error");
+//        break;
+//}
+
+
+//string fruit=Console.ReadLine();
+//switch (fruit)
+//{
+//    case "banana":
+//    case "apple":
+//    case "kiwi":
+//    case "cherry":
+//    case "lemon":
+//    case "grapes":
+//        Console.WriteLine("fruit");
+//        break;
+//    case "tomato":
+//    case "cucumber":
+//    case "papper":
+//    case "carrot":
+//        Console.WriteLine("vegetable");
+//        break;
+//    default:
+//        Console.WriteLine("error");
+//        break;
+//}
+
+//int age = int.Parse(Console.ReadLine());
+//string gender = Console.ReadLine();
+
+//if (gender == "f")
+//{
+//    if (age >= 16)
+//    {
+//        Console.WriteLine("Ms");
+//    }
+//    else
+//    {
+//    Console.WriteLine("Miss");
+//      }
+//}
+//else if (gender == "m")
+//{
+//    if (age >= 16)
+//    {
+//        Console.WriteLine("Mr");
+//    }
+//    else
+//    {
+//         Console.WriteLine("Master");
+//    }
+//}
+
+
+//int a = int.Parse(Console.ReadLine());
+//if (a > -100 && a < 100 && a != 0)
+//{
+//    Console.WriteLine("yes");
+//}
+//else
+//{
+//    Console.WriteLine("no");
+//}
+
+
+//string day = Console.ReadLine();
+//if (day == "Monday" || day == "Tuesday" || day == "Friday")
+//{
+//    Console.WriteLine("12");
+//}
+//else if(day=="Wednesday"||day=="Thursday")
+//{
+//    Console.WriteLine("14");
+//}
+//else if (day == "Saturdau" || day == "Sunday")
+//{
+//    Console.WriteLine("16");
+//}
