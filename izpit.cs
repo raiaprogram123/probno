7e4e0175aaaa07664c01f517654aec556e933d48
raiaprogram123 <raiadragolova@gmail.com>
2025-12-04 12:32:52 +0200

    Add files via upload

---
+++ izpit.cs
@@ -0,0 +1,21 @@
+﻿// Определете вида на триъгълника
+
+int a=int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a + b > c && a + c > b && b + c > a)
+{
+    if (a == b && b == c)
+    {
+        Console.WriteLine("Ravnostranen");
+    }
+    else if (a == b ||a==c || b==c)
+    {
+        Console.WriteLine("Ravnobedren");
+    }
+    else
+    {
+        Console.WriteLine("Raznostranen");
+    }
+}
